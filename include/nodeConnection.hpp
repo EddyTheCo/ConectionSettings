51df16c3173f58a94a038d05223a236f8f22ac62
EddyGlezz <eddyglezz95@gmail.com>
2023-03-08 20:19:40 +0100

    add a static function that return the object pointer of the singleton and add a property to get the hrp in qml

--- include/nodeConnection.hpp
+++ include/nodeConnection.hpp
@@ -12,11 +12,12 @@ class Node_Conection: public QObject
 {
     Q_OBJECT
 
-       Q_PROPERTY(QUrl  nodeaddr READ nodeaddr WRITE set_naddr NOTIFY naddrChanged)
-       Q_PROPERTY(QString  jwt READ jwt WRITE setjwt NOTIFY jwtChanged)
-       Q_PROPERTY(ConState  state READ state NOTIFY stateChanged)
-       QML_ELEMENT
-       QML_SINGLETON
+    Q_PROPERTY(QUrl  nodeaddr READ nodeaddr WRITE set_naddr NOTIFY naddrChanged)
+    Q_PROPERTY(QString  jwt READ jwt WRITE setjwt NOTIFY jwtChanged)
+    Q_PROPERTY(ConState  state READ state NOTIFY stateChanged)
+    Q_PROPERTY(QString  hrp READ hrp NOTIFY hrpChanged)
+    QML_ELEMENT
+    QML_SINGLETON
 
 public:
 
@@ -27,21 +28,26 @@ public:
     };
     Q_ENUM(ConState)
     QUrl nodeaddr()const{return rest_client->get_node_address();}
-    QString jwt()const{return rest_client->get_jwt();}
+    static QString jwt(){return rest_client->get_jwt();}
     void set_naddr(QUrl naddr){rest_client->set_node_address(naddr);}
     void setjwt(QString jwt_){rest_client->set_jwt(jwt_);}
     static qiota::Client* rest_client;
     static qiota::ClientMqtt * mqtt_client;
     static ConState state(void){return state_;}
+    static QString hrp(){return hrp_;}
+    static Node_Conection * ptr(void){return ptr_;}
 
 signals:
     void naddrChanged();
     void jwtChanged();
     void stateChanged(Node_Conection::ConState);
+    void hrpChanged();
 
 private:
     void set_node_addr_wss(const QUrl wss);
     static ConState state_;
+    static QString hrp_;
+    static Node_Conection *ptr_;
 
 
 };
--- src/nodeConnection.cpp
+++ src/nodeConnection.cpp
@@ -8,9 +8,10 @@ qiota::Client* Node_Conection::rest_client=nullptr;
 qiota::ClientMqtt * Node_Conection::mqtt_client=nullptr;
 
 Node_Conection::ConState Node_Conection::state_=Node_Conection::Disconnected;
-
+Node_Conection*  Node_Conection::ptr_=nullptr;
 Node_Conection::Node_Conection()
 {
+    if(!ptr_)ptr=this;
     rest_client=new Client(this);
     mqtt_client=new ClientMqtt(this);
 
@@ -19,8 +20,13 @@ Node_Conection::Node_Conection()
         if(rest_client->state()==Client::ClientState::Connected)
         {
             set_node_addr_wss(rest_client->get_node_address());
-            state_=Connected;
-            emit stateChanged(state_);
+            auto info=rest_client->get_api_core_v2_info();
+            QObject::connect(info,&Node_info::finished,this,[=]( ){
+                this->hrp_=info->bech32Hrp;
+                state_=Connected;
+                emit stateChanged(state_);
+                info->deleteLater();
+            });
         }
     });
 

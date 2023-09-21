9e55ea2bbdfe72f4a553371e0e0ffe18719b1132
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-09-21 10:34:41 +0200

    expose the instance as static

--- src/nodeConnection.cpp
+++ src/nodeConnection.cpp
@@ -3,16 +3,12 @@
 
 using namespace qiota;
 
-qiota::Client* Node_Conection::rest_client=new Client();
 
-qiota::ClientMqtt * Node_Conection::mqtt_client=new ClientMqtt();
-
-Node_Conection::ConState Node_Conection::state_=Node_Conection::Disconnected;
-
-Node_Conection::Node_Conection(QObject *parent):QObject(parent)
+Node_Conection * Node_Conection::m_instance=nullptr;
+Node_Conection::Node_Conection(QObject *parent):QObject(parent),rest_client(new Client(this)),mqtt_client(new ClientMqtt(this)),
+    state_(Node_Conection::Disconnected)
 {
-    rest_client->setParent(this);
-    mqtt_client->setParent(this);
+    if(!m_instance)m_instance=this;
     connect(rest_client,&qiota::Client::stateChanged,this,[=]()
     {
         if(rest_client->state()==Client::ClientState::Connected)
